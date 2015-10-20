77a18e3f548ca45dbb41bbe3780628fdca020329
unknown <info@webkleur.com>
2015-10-20 16:54:23 +0200

    fetch data by ->find (based on primary key)

--- src/AppBundle/Controller/TwitterController.php
+++ src/AppBundle/Controller/TwitterController.php
@@ -6,22 +6,41 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class TwitterController extends Controller
 {
     /**
-     * @Route("/twitter")
+     * @Route("/twitter/{val}")
      */
-    public function indexAction()
+    public function indexAction($val)
     {
-        $product = new product();
-        $product->setName('bobie')
+//lame insert - for god knows what reason ^^
+/*  $product = new product();
+            $product->setName('bobie')
             ->setPrice("10")
             ->setDescription("description hier");
-
     $em = $this->getDoctrine()->getManager();
     $em->persist($product);
     $em->flush();
+    */
+//retrieve data   
+  $product = new product(); 
+  $repository = $this->getDoctrine()
+    ->getRepository('AppBundle:Product');
+
+   $product = $repository->find(10);
+
+  
+//parse to twigie, pass $val with twigie
+      return $this->render(
+        'twitter/twitter.html.twig',
+        array('product' => $product,
+            'value' => $val
+        
+             )
+        );
      
 
     }
