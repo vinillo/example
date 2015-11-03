a7dfbffc24003cc4297c9d9306d802700cbf1e6c
Vin <info@webkleur.com>
2015-11-03 10:38:16 +0100

    * added UserBundle

---
+++ src/UserBundle/Controller/SecuirityController.php
@@ -0,0 +1,17 @@
+<?php
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/login", name="login_form")
+ */
+class SecurityController
+{
+    public function loginAction()
+    {
+
+    }
+
+}
---
+++ src/UserBundle/UserBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
